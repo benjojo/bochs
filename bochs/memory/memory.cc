949900ca66a046f61107e7f190290bef1c326a01
Ben Cox <ben@benjojo.co.uk>
2013-11-12 21:04:01 +0000

    Fixed breaking build

--- bochs/memory/memory.cc
+++ bochs/memory/memory.cc
@@ -44,9 +44,9 @@ void BX_MEM_C::writePhysicalPage(BX_CPU_C *cpu, bx_phy_address addr, unsigned le
   Bit8u *data_ptr;
   bx_phy_address a20addr = A20ADDR(addr);
   struct memory_handler_struct *memory_handler = NULL;
-  if(lastsnapshot < time()) {
+  if(lastsnapshot < time(0)) {
     // We need to make a snapshot now.
-    lastsnapshot = time();
+    lastsnapshot = time(0);
   }
   // Note: accesses should always be contained within a single page
   if ((addr>>12) != ((addr+len-1)>>12)) {
